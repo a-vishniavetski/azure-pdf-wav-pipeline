79a8a9e6f31eedd27d3477a54a946b1efd47bee3
avish <a.vishniavetski@gmail.com>
2023-10-13 20:48:36 +0200

    feat:Cognitive Search index creation and population done (index_update.py)

--- document_intelligence/document_analysis.py
+++ document_intelligence/document_analysis.py
@@ -6,32 +6,47 @@ from azure.core.credentials import AzureKeyCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 
-# set credentials to access the service. 
-# Environment vars are not very safe, but for test purposes they will do.
-DI_KEY = os.environ.get("DI_KEY1")
-DI_ENDPOINT = os.environ.get("DI_ENDPOINT")
-STORAGE_STRING = os.environ.get("STORAGE_STRING1")
-ACCOUNT_URL = "https://avishpdfstorage.blob.core.windows.net"
-CONTAINER_NAME = "pdfs"
+def analyze_general_documents(doc_infos: dict[str, str],
+                              di_endpoint: str,
+                              di_key: str) -> list[dict[str, str]]:
+    """
+    Analyzes the content of general documents using Azure Form Recognizer.
 
-def analyze_general_documents(doc_infos: dict) -> dict:
-    retval = {}
+    Args:
+        doc_infos (dict): A dictionary of document names and their corresponding URLs.
 
-    # document
-    docUrl = "https://avishpdfstorage.blob.core.windows.net/pdfs/Ancient_Roman_architecture.pdf"
+    Returns:
+        dict: A dictionary of document names and the extracted text from each document.
+    """
+
+    retval = []
 
     # create your `DocumentAnalysisClient` instance and `AzureKeyCredential` variable
-    document_analysis_client = DocumentAnalysisClient(endpoint=DI_ENDPOINT, credential=AzureKeyCredential(DI_KEY))
+    document_analysis_client = DocumentAnalysisClient(endpoint=di_endpoint, credential=AzureKeyCredential(di_key))
 
     for name, url in doc_infos.items():
         doc_text = analyze_general_document(document_analysis_client=document_analysis_client,
                                             url=url)
-        retval[name] = doc_text
+        _dict = {}
+        _dict["name"] = name
+        _dict["information"] = doc_text
+        retval.append(_dict)
 
     return retval
 
 
-def analyze_general_document(document_analysis_client: DocumentAnalysisClient, url: str):
+def analyze_general_document(document_analysis_client: DocumentAnalysisClient, url: str) -> str:
+    """
+    Analyzes the content of a specific document using Azure Form Recognizer.
+
+    Args:
+        document_analysis_client (DocumentAnalysisClient): An instance of DocumentAnalysisClient.
+        url (str): The URL of the document to be analyzed.
+
+    Returns:
+        str: The extracted text from the document.
+    """
+
     retval = ""
 
     poller = document_analysis_client.begin_analyze_document_from_url(
@@ -41,16 +56,29 @@ def analyze_general_document(document_analysis_client: DocumentAnalysisClient, u
 
     for page in result.pages:
         retval = "".join(line.content for line in page.lines)
-        
+    
+    print(f"Document analyzed successfully. URL: {url}")
     return retval
 
 
-def get_blob_infos(storage_account_string: str, container_name: str) -> dict:
+def get_blob_infos(storage_account_string: str, 
+                   container_name: str,
+                   ) -> dict[str, str]:
+    """
+    Retrieves a dictionary of blob names and their corresponding URLs from an Azure Blob Storage account.
+
+    Args:
+        storage_account_string (str): The connection string for the Azure Storage Account.
+        container_name (str): The name of the container in the Azure Blob Storage.
+
+    Returns:
+        dict: A dictionary containing blob names as keys and their URLs as values.
+    """
+    
     # ACCESS THE STORAGE ACCOUNT
-    blob_service_client = BlobServiceClient.from_connection_string(STORAGE_STRING)
+    blob_service_client = BlobServiceClient.from_connection_string(storage_account_string)
     
     # ACCES THE CONTAINER
-    container_name = "pdfs"    
     container_client = blob_service_client.get_container_client(container=container_name)
 
     # ACCESS THE BLOBS, GET THEIR URLS
@@ -59,24 +87,7 @@ def get_blob_infos(storage_account_string: str, container_name: str) -> dict:
     for blob_property in blobs_properties:
         blob_client = container_client.get_blob_client(blob=blob_property.name)
         blob_url = blob_client.url
-        blob_name = blob_client.blob_name
+        blob_name = blob_client.blob_name[:blob_client.blob_name.find(".pdf")].replace("-", " ")
         blob_info[blob_name] = blob_url
 
     return blob_info
-
-
-if __name__ == "__main__":
-    documents_information = get_blob_infos(STORAGE_STRING, CONTAINER_NAME)
-
-    documents_analysed = analyze_general_documents(doc_infos=documents_information)
-    for name, text in documents_analysed.items():
-        print(name, text, sep='\n')
-        print('\n')
-
-
-
-    #container_client.get_blob_client(blob=)
-    # blob_service_client.get_blob_client(container=container_name, blob=)
-
-
-    
